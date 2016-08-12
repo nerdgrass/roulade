5c623a33daa45dee5ed901ab68fd040da05580b1
Alex B <arboerstler@gmail.com>
2016-08-12 12:11:24 -0400

    okay, no, really fixing the package.json issue now. Honest.

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import babel from 'rollup-plugin-babel';
 
 export default {
     entry: 'src/index',
-    dest: 'dist/app.js',
+    dest: 'dist/roulade.js',
     format: 'cjs',
 
     // Transpile any ES2015 in the output file.
     plugins: [babel()],
 
     // Make the output executable.
-    banner: '#!/usr/bin/env node'
-};
+//     banner: '#!/usr/bin/env node'
+ };
